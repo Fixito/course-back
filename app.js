edbad3625ebfe9ac0acf59f4717898ac19cbdaf7
Fixito <thomas_3004@hotmail.fr>
2022-04-02 08:38:38 +0200

    init

---
+++ app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+
+app.use(express.json());
+
+//* error handlers
+const notFoundMiddleware = require('./middleware/not-found');
+
+//* routes
+app.use('/api/v1/guides', (req, res) => {
+  res.send('hello world');
+});
+
+app.use(notFoundMiddleware);
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ middleware/not-found.js
@@ -0,0 +1,3 @@
+const notFound = (req, res) => res.send('<h1>404 page not found</h1>');
+
+module.exports = notFound;
